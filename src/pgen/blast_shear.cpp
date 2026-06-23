14f389e15b53732c3a0702e70af59f3ff69e90ee
Morgan MacLeod <morganmacleod@gmail.com>
2026-06-23 11:38:44 -0400

    adding blast problem for EM in which we can add shearing box to later

---
+++ src/pgen/blast_shear.cpp
@@ -0,0 +1,138 @@
+//========================================================================================
+// Athena++ astrophysical MHD code
+// Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
+// Licensed under the 3-clause BSD License, see LICENSE file for details
+//========================================================================================
+//! \file blast.cpp
+//! \brief Problem generator for spherical blast wave problem.  Works in Cartesian,
+//!        cylindrical, and spherical coordinates.  Contains post-processing code
+//!        to check whether blast is spherical for regression tests
+//!
+//! REFERENCE: P. Londrillo & L. Del Zanna, "High-order upwind schemes for
+//!   multidimensional MHD", ApJ, 530, 508 (2000), and references therein.
+
+// C headers
+
+// C++ headers
+#include <algorithm>
+#include <cmath>
+#include <cstdio>     // fopen(), fprintf(), freopen()
+#include <cstring>    // strcmp()
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+// Athena++ headers
+#include "../athena.hpp"
+#include "../athena_arrays.hpp"
+#include "../coordinates/coordinates.hpp"
+#include "../eos/eos.hpp"
+#include "../field/field.hpp"
+#include "../globals.hpp"
+#include "../hydro/hydro.hpp"
+#include "../mesh/mesh.hpp"
+#include "../parameter_input.hpp"
+
+Real threshold;
+
+int RefinementCondition(MeshBlock *pmb);
+
+void Mesh::InitUserMeshData(ParameterInput *pin) {
+  if (adaptive) {
+    EnrollUserRefinementCondition(RefinementCondition);
+    threshold = pin->GetReal("problem","thr");
+  }
+  return;
+}
+
+//========================================================================================
+//! \fn void MeshBlock::ProblemGenerator(ParameterInput *pin)
+//! \brief Spherical blast wave test problem generator
+//========================================================================================
+
+void MeshBlock::ProblemGenerator(ParameterInput *pin) {
+  Real rout = pin->GetReal("problem", "radius");
+  Real rin  = rout - pin->GetOrAddReal("problem", "ramp", 0.0);
+  Real pa   = pin->GetOrAddReal("problem", "pamb", 1.0);
+  Real da   = pin->GetOrAddReal("problem", "damb", 1.0);
+  Real prat = pin->GetReal("problem", "prat");
+  Real drat = pin->GetOrAddReal("problem", "drat", 1.0);
+  Real gamma = peos->GetGamma();
+  Real gm1 = gamma - 1.0;
+
+  // setup uniform ambient medium with spherical over-pressured region
+  for (int k=ks; k<=ke; k++) {
+    for (int j=js; j<=je; j++) {
+      for (int i=is; i<=ie; i++) {
+        Real rad;
+	Real x = pcoord->x1v(i);
+	Real y = pcoord->x2v(j);
+	Real z = pcoord->x3v(k);
+	rad = std::sqrt(SQR(x) + SQR(y) + SQR(z));
+        
+        Real den = da;
+        if (rad < rout) {
+          if (rad < rin) {
+            den = drat*da;
+          } else {   // add smooth ramp in density
+            Real f = (rad-rin) / (rout-rin);
+            Real log_den = (1.0-f) * std::log(drat*da) + f * std::log(da);
+            den = std::exp(log_den);
+          }
+        }
+
+        phydro->u(IDN,k,j,i) = den;
+        phydro->u(IM1,k,j,i) = 0.0;
+        phydro->u(IM2,k,j,i) = 0.0;
+        phydro->u(IM3,k,j,i) = 0.0;
+        if (NON_BAROTROPIC_EOS) {
+          Real pres = pa;
+          if (rad < rout) {
+            if (rad < rin) {
+              pres = prat*pa;
+            } else {  // add smooth ramp in pressure
+              Real f = (rad-rin) / (rout-rin);
+              Real log_pres = (1.0-f) * std::log(prat*pa) + f * std::log(pa);
+              pres = std::exp(log_pres);
+            }
+          }
+          phydro->u(IEN,k,j,i) = pres/gm1;
+	}
+      }
+    }
+  }
+
+}
+
+// refinement condition: check the maximum pressure gradient
+int RefinementCondition(MeshBlock *pmb) {
+  AthenaArray<Real> &w = pmb->phydro->w;
+  Real maxeps = 0.0;
+  if (pmb->pmy_mesh->f3) {
+    for (int k=pmb->ks-1; k<=pmb->ke+1; k++) {
+      for (int j=pmb->js-1; j<=pmb->je+1; j++) {
+        for (int i=pmb->is-1; i<=pmb->ie+1; i++) {
+          Real eps = std::sqrt(SQR(0.5*(w(IPR,k,j,i+1) - w(IPR,k,j,i-1)))
+                               +SQR(0.5*(w(IPR,k,j+1,i) - w(IPR,k,j-1,i)))
+                               +SQR(0.5*(w(IPR,k+1,j,i) - w(IPR,k-1,j,i))))/w(IPR,k,j,i);
+          maxeps = std::max(maxeps, eps);
+        }
+      }
+    }
+  } else if (pmb->pmy_mesh->f2) {
+    int k = pmb->ks;
+    for (int j=pmb->js-1; j<=pmb->je+1; j++) {
+      for (int i=pmb->is-1; i<=pmb->ie+1; i++) {
+        Real eps = std::sqrt(SQR(0.5*(w(IPR,k,j,i+1) - w(IPR,k,j,i-1)))
+                             + SQR(0.5*(w(IPR,k,j+1,i) - w(IPR,k,j-1,i))))/w(IPR,k,j,i);
+        maxeps = std::max(maxeps, eps);
+      }
+    }
+  } else {
+    return 0;
+  }
+
+  if (maxeps > threshold) return 1;
+  if (maxeps < 0.25*threshold) return -1;
+  return 0;
+}
